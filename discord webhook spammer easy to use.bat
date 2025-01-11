3543b901badfd6dc62fd911777031e10b386db98
nipoto <nipotoyt@gmail.com>
2025-01-11 21:30:14 +0200

    discord webhook spammer
    a 1kb bat file wich spam a discord webhook

---
+++ discord webhook spammer easy to use.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:main
+cls
+set "webhook="
+set "message="
+set "count="
+
+if exist webhook.txt (
+    set /p "use_last_webhook=Do you want to use the last webhook? (y/n): "
+    if /i "!use_last_webhook!"=="y" (
+        set /p "webhook="<webhook.txt
+    )
+)
+
+if not defined webhook (
+    set /p "webhook=What is the discord webhook? "
+    echo !webhook!>webhook.txt
+)
+
+set /p "message=What message do you wanna send? "
+set /p "count=How many times to send that message? (min 1) (max 1000) "
+
+for /l %%i in (1,1,!count!) do (
+    curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"!message!\"}" !webhook!
+    echo Sent %%i
+)
+
+set /p "repeat=Do you want to do it again? (y/n): "
+if /i "!repeat!"=="y" (
+    goto main
+)
+
+endlocal
+exit
